75bf2cf3a75f4e2bb723efdd418e7aaa5d0db5cf
mateusluizz <mateusluizz@hotmail.com>
2023-12-18 23:13:55 -0400

    H2 Config, Entiries (User, Order and OrderStatus)

--- src/main/java/com/devsuperior/dscommerce/DscommerceApplication.java
+++ src/main/java/com/devsuperior/dscommerce/DscommerceApplication.java
@@ -1,13 +1,14 @@
 package com.devsuperior.dscommerce;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DscommerceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DscommerceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DscommerceApplication.class, args);
+    }
 
 }
