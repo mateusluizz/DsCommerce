1a547d3ac58dbdae585d55cad810d9fd97be1e61
mateusluizz <mateusluizz@hotmail.com>
2023-12-23 14:43:23 -0400

    Sql seeds

--- src/main/java/com/devsuperior/dscommerce/entities/Product.java
+++ src/main/java/com/devsuperior/dscommerce/entities/Product.java
@@ -10,7 +10,7 @@ import java.util.Set;
 @Table(name = "tb_product")
 public class Product {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     @Column(columnDefinition = "TEXT")
